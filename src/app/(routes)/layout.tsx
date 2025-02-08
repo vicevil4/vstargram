ec86f2be8ff54a43355bc9dccbc2d0b808426943
Simon Jung <vicevil4@gmail.com>
2025-02-09 00:02:52 +0900

    add side desktop navigation

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -4,6 +4,10 @@ import "../globals.css";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 import MobileNav from "@/components/MobileNav";
+import logo from "../logo.png";
+import Image from "next/image";
+import { CameraIcon, HomeIcon, LayoutGridIcon, SearchIcon, UserIcon } from "lucide-react";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +31,43 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased p-4`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Theme>
-          {children}
+          <div className="flex">
+            <div className="hidden lg:block p-4 w-48 shadow-md shadow-gray-400">
+              <div className="top-0 sticky">
+                <Image className="w-full" src={logo} alt="" />
+                <div className="ml-1 inline-flex flex-col gap-4 mt-4 *:flex *:items-center *:gap-2">
+                  <Link href="/">
+                    <HomeIcon />
+                    Home
+                  </Link>
+                  <Link href="/search">
+                    <SearchIcon />
+                    Search
+                  </Link>
+                  <Link href="/browse">
+                    <LayoutGridIcon />
+                    Browse
+                  </Link>
+                  <Link href="/profile">
+                    <UserIcon />
+                    Profile
+                  </Link>
+                  <Link href="/create">
+                    <CameraIcon />
+                    Create
+                  </Link>
+                </div>
+              </div>
+              
+            </div>
+            <div className="p-4 flex justify-around w-full">
+              <div>
+                {children}
+              </div>
+            </div>
+          </div>
           <MobileNav />
         </Theme>
       </body>
