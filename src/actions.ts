32e0cc7e3a9eb4e2ae78b5fc6505211bf1caec73
Simon Jung <vicevil4@gmail.com>
2025-02-24 22:47:56 +0900

    Add like functionality to posts with database model and UI components

--- src/actions.ts
+++ src/actions.ts
@@ -2,56 +2,90 @@
 import { prisma } from "@/db";
 import { auth } from "./auth";
 
-async function getSessionEmailOrThrow() {
-    const session = await auth();
-    const userEmail = session?.user?.email;
-    if (!userEmail) {
-        throw "not logged in";
-    }
-    return userEmail;
+export async function getSessionEmailOrThrow() {
+  const session = await auth();
+  const userEmail = session?.user?.email;
+  if (!userEmail) {
+    throw "not logged in";
+  }
+  return userEmail;
 }
 
 export async function updateProfile(data: FormData) {
-    const userEmail = await getSessionEmailOrThrow();
-    const newUserInfo = {
-        username: data.get("username") as string,
-        name: data.get("name") as string,
-        subtitle: data.get("subtitle") as string,
-        bio: data.get("bio") as string,
-        avatar: data.get("avatar") as string,
-    }
-    await prisma.profile.upsert({
-        where: {
-            email: userEmail,
-        },
-        update: newUserInfo,
-        create: {
-            email: userEmail,
-            ...newUserInfo,
-        },
-    });
+  const userEmail = await getSessionEmailOrThrow();
+  const newUserInfo = {
+    username: data.get("username") as string,
+    name: data.get("name") as string,
+    subtitle: data.get("subtitle") as string,
+    bio: data.get("bio") as string,
+    avatar: data.get("avatar") as string,
+  };
+  await prisma.profile.upsert({
+    where: {
+      email: userEmail,
+    },
+    update: newUserInfo,
+    create: {
+      email: userEmail,
+      ...newUserInfo,
+    },
+  });
 }
 
 export async function postEntry(data: FormData) {
-    const userEmail = await getSessionEmailOrThrow();
-    const postDoc = await prisma.post.create({
-        data: {
-            author: userEmail,
-            image: data.get("image") as string,
-            description: data.get("description") as string || "",
-        }
-    });
-    return postDoc.id;
+  const userEmail = await getSessionEmailOrThrow();
+  const postDoc = await prisma.post.create({
+    data: {
+      author: userEmail,
+      image: data.get("image") as string,
+      description: (data.get("description") as string) || "",
+    },
+  });
+  return postDoc.id;
 }
 
 export async function postComment(data: FormData) {
-    const authorEmail = await getSessionEmailOrThrow();
-    return prisma.comment.create({
-        data: {
-            author: authorEmail,
-            postId: data.get('postId') as string,
-            text: data.get('text') as string
-        }
-    })
+  const authorEmail = await getSessionEmailOrThrow();
+  return prisma.comment.create({
+    data: {
+      author: authorEmail,
+      postId: data.get("postId") as string,
+      text: data.get("text") as string,
+    },
+  });
 }
 
+async function updatePostLikeCount(postId: string) {
+  await prisma.post.update({
+    where: {
+      id: postId,
+    },
+    data: {
+      likeCount: await prisma.like.count({ where: { postId: postId } }),
+    },
+  });
+}
+
+export async function likePost(data: FormData) {
+  const authorEmail = await getSessionEmailOrThrow();
+  const postId = data.get("postId") as string;
+  await prisma.like.create({
+    data: {
+      author: authorEmail,
+      postId: postId,
+    },
+  });
+  await updatePostLikeCount(postId);
+}
+
+export async function removeLikeFromPost(data: FormData) {
+  const authorEmail = await getSessionEmailOrThrow();
+  const postId = data.get("postId") as string;
+  await prisma.like.deleteMany({
+    where: {
+      author: authorEmail,
+      postId: postId,
+    },
+  });
+  await updatePostLikeCount(postId);
+}
--- src/app/(routes)/posts/[id]/page.tsx
+++ src/app/(routes)/posts/[id]/page.tsx
@@ -1,40 +1,57 @@
 import Comment from "@/components/Comment";
 import SessionCommentForm from "@/components/SessionCommentForm";
 import { Suspense } from "react";
-import {uniq} from "lodash";
+import { uniq } from "lodash";
+import { BookmarkIcon, HeartIcon } from "lucide-react";
+import LikesInfo from "@/components/LikesInfo";
+import { getSessionEmailOrThrow } from "@/actions";
 
-export default async function SinglePostPage({params}:{params:{id:string}}) {
-  const post = await prisma?.post.findFirstOrThrow({where:{id:params.id}});
-  const authorProfile = await prisma?.profile.findFirstOrThrow({where:{email:post?.author}});
-  const comments = await prisma?.comment.findMany({where:{postId:post.id}});
+export default async function SinglePostPage({ params }: { params: { id: string } }) {
+  const post = await prisma?.post.findFirstOrThrow({ where: { id: params.id } });
+  const authorProfile = await prisma?.profile.findFirstOrThrow({ where: { email: post?.author } });
+  const comments = await prisma?.comment.findMany({ where: { postId: post.id } });
   const commetnsAuthors = await prisma?.profile.findMany({
     where: {
-      email: {in: uniq(comments?.map(c => c.author))}
+      email: { in: uniq(comments?.map(c => c.author)) }
     }
-  })
+  });
+  const myLike = await prisma?.like.findFirst({
+    where: {
+      author: await getSessionEmailOrThrow(),
+      postId: post?.id,
+    }
+  });
   return (
     <div>
       <div className="grid md:grid-cols-2 gap-4">
         <div>
-          <img 
+          <img
             className="rounded-md"
             src={post?.image} alt={post?.description} />
         </div>
         <div>
-          <Comment 
+          <Comment
             createdAt={post?.createdAt}
             text={post?.description} authorProfile={authorProfile} />
           <div className="pt-4 flex flex-col gap-4">
             {comments?.map(comment => (
               <div key={comment.id}>
-                <Comment 
+                <Comment
                   createdAt={comment.createdAt}
-                  text={comment.text} 
+                  text={comment.text}
                   authorProfile={commetnsAuthors?.find(a => a.email === comment.author)} />
               </div>
             ))}
           </div>
-          <div className="pt-8 border-t mt-8 border-t-gray-300">
+          <div className="flex items-center gap-2 text-gray-700 justify-between py-4 mt-4 border-t border-t-gray-300">
+            <LikesInfo sessionLike={myLike} post={post} />
+            <div className="flex items-center">
+              <button>
+                <BookmarkIcon />
+              </button>
+            </div>
+          </div>
+          <div className="pt-8 border-t border-t-gray-300">
             <Suspense>
               <SessionCommentForm postId={post.id} />
             </Suspense>
